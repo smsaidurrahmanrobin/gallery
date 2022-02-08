8bd55c4f8572a7808b4ded536424cf5bb163dae1
smsaidurrahmanrobin <smsaiyeed@gmail.com>
2022-02-08 21:56:12 +0600

    End of Section:6_galleryCMS_OOP
    End of Section:6_galleryCMS_OOP

--- admin/login.php
+++ admin/login.php
@@ -43,6 +43,7 @@ if(isset($_POST['submit'])){
     
     $username = "";
     $password = "";
+    $the_message = "";
     
 }
 
@@ -54,7 +55,7 @@ if(isset($_POST['submit'])){
 
 <div class="col-md-4 col-md-offset-3">
 
-<h4 class="bg-danger"><?php //echo $the_message; ?></h4>
+<h4 class="bg-danger"><?php echo $the_message; ?></h4>
 	
 <form id="login-id" action="" method="post">
 	
